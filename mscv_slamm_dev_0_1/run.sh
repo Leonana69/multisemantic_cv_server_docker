23aaecccfd55deff14ac242582048ea53ef819ec
leonana69 <chengj0803@gmail.com>
2023-03-29 22:37:41 -0400

    load duke_drone seq

--- mscv_slamm_dev_0_1/run.sh
+++ mscv_slamm_dev_0_1/run.sh
@@ -4,4 +4,5 @@ docker run -td --privileged --net=host --ipc=host \
     -e "DISPLAY=$DISPLAY" \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
     -v /home/gc635/Downloads/data_odometry_gray/dataset/sequences:/data_odometry_gray/dataset/sequences \
+    -v /home/gc635/Downloads/duke_drone:/duke_drone \
     mscv-slamm-dev:0.1 \
