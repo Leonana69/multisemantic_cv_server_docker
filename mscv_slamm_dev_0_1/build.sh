9d8a349d6dbeba057c02a272d2472adebef32509
leonana69 <chengj0803@gmail.com>
2023-04-17 16:16:08 -0400

    add GUI permission

--- mscv_slamm_dev_0_1/build.sh
+++ mscv_slamm_dev_0_1/build.sh
@@ -1,3 +1,11 @@
+# UI permisions
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+xhost +local:docker
+
 # remove old container and image
 docker stop -t 0 mscv-slamm-dev
 docker image rm -f mscv-slamm-dev:0.1
