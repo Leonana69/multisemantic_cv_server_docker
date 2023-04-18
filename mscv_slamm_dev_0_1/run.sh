dc860fe4fbf7940c54cbb1511f5d006fce218a9e
leonana69 <chengj0803@gmail.com>
2023-04-18 15:40:58 -0400

    update slamm eigen to 3.2.10 to solve seg fault issue

--- mscv_slamm_dev_0_1/run.sh
+++ mscv_slamm_dev_0_1/run.sh
@@ -1,8 +1,10 @@
 # create a new container
 docker run -td --privileged --net=host --ipc=host \
+    --memory=2g \
     --name="mscv-slamm-dev" \
     -e "DISPLAY=$DISPLAY" \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-    -v /home/gc635/Downloads/data_odometry_gray/dataset/sequences:/data_odometry_gray/dataset/sequences \
-    -v /home/gc635/Downloads/duke_drone:/duke_drone \
+    -v /home/gc635/Downloads/duke_drone_angle:/duke_drone \
     mscv-slamm-dev:0.1 \
+
+    #-v /home/gc635/Downloads/data_odometry_gray/dataset/sequences:/data_odometry_gray/dataset/sequences \
