acbac7847c39af678ebba02bef02a2f26ad1595c
leonana69 <chengj0803@gmail.com>
2023-04-25 14:27:22 -0400

    update slam dev

--- mscv_slam_dev_0_1/open_2.sh
+++
@@ -1,2 +0,0 @@
-# open the container
-docker exec -it mscv-slam-dev-2 bash -c "./src/build/mscv_slam_dev_service ./Dev/ORB_SLAM3/Vocabulary/ORBvoc.txt ./Dev/ORB_SLAM3/Examples/Monocular/mbp14.yaml 50006"
--- mscv_slam_dev_0_1/run_2.sh
+++
@@ -1,6 +0,0 @@
-# create a new container
-docker run -td --privileged --net=host --ipc=host \
-    --name="mscv-slam-dev-2" \
-    -e "DISPLAY=$DISPLAY" \
-    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-    mscv-slam-dev:0.1 \
